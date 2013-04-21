9a5c28f1c28ba35db6ada1ab6f4b548f50812b82
Justin Valentini <justin.valentini@gmail.com>
2013-04-21 15:23:34 -0400

    Tests for the simplest possible case (slime)

--- spec/canattr_integration_spec.rb
+++ spec/canattr_integration_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
-describe 'Canattr' do
-  it 'modifies writeable fields' do
-    pending
+class CanAttr
+  attr_accessor :name, :password
+
+  def password=(password)
+  end
+end
+
+describe 'CanAttr' do
+  it 'allows setting the name field' do
+    user = CanAttr.new
+    user.name = 'John'
+
+    expect(user.name).to eq('John')
+  end
+
+  it 'fails to set the password field' do
+    user = CanAttr.new
+    user.password = 'abc123'
+
+    expect(user.password).not_to eq('abc123')
   end
 end
